78c4e2284406c8f771ed411a3ee3b5be14811ed5
veerareddy14 <veerayasam7@gmail.com>
2021-07-11 11:41:35 +0530

    on zoomchange deactivated

--- src/index.js
+++ src/index.js
@@ -110,9 +110,9 @@ class MapView extends Component {
 					handleMapMounted={this.handleMapMounted}
 					containerElement={<div style={{ height: "100%" }} />}
 					mapElement={<div style={{ height: "100%" }} />}
-					onZoomChanged={() => {
-						this.setState({ zoom: this.map.getZoom() });
-					}}
+					// onZoomChanged={() => {
+					// 	this.setState({ zoom: this.map.getZoom() });
+					// }}
 					{...googleMapProps}
 					onDragStart={onRegionChange}
 					onIdle={this.onDragEnd}
