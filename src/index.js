eee3c339a3d46013eda10e8f21085c562224d784
veerareddy14 <veerayasam7@gmail.com>
2021-07-14 00:06:38 +0530

    latDelta and lonDelta functional

--- src/index.js
+++ src/index.js
@@ -22,77 +22,73 @@ class MapView extends Component {
 		this.props.onMapReady && this.props.onMapReady();
 	};
 
-	getCamera = () => {
-		console.log("i am in getCamera");
-		return {
-			zoom: this.map.getZoom(),
-			center: this.map.getCenter(),
-			heading: this.map.getHeading(),
-		};
-	};
+	// getCamera = () => {
+	// 	console.log("i am in getCamera");
+	// 	return {
+	// 		zoom: this.map.getZoom(),
+	// 		center: this.map.getCenter(),
+	// 		heading: this.map.getHeading(),
+	// 	};
+	// };
 
-	animateCamera(camera) {
-		console.log("i am in animateCamera");
-		this.setState({ zoom: camera.zoom });
-		this.setState({ center: camera.center });
-	}
+	// animateCamera(camera) {
+	// 	console.log("i am in animateCamera");
+	// 	this.setState({ zoom: camera.zoom });
+	// 	this.setState({ center: camera.center });
+	// }
 
-	animateToRegion(coordinates) {
-		console.log("i am in animateToRegion");
-		this.setState({
-			center: { lat: coordinates.latitude, lng: coordinates.longitude },
-		});
+	// animateToRegion(coordinates) {
+	// 	console.log("i am in animateToRegion");
+	// 	this.setState({
+	// 		center: { lat: coordinates.latitude, lng: coordinates.longitude },
+	// 	});
+	// }
 
-		// this.map.panTo({ lat: coordinates.latitude, lng: coordinates.longitude });
-	}
+	// _getCurrentRegion = () => {
+	// 	const center = this.map.getCenter();
+	// 	const bounds = this.map.getBounds();
+	// 	const zoom = this.map.getZoom();
+	// 	console.log(zoom);
 
-	_getCurrentRegion = () => {
-		const center = this.map.getCenter();
-		const bounds = this.map.getBounds();
-		const zoom = this.map.getZoom();
-		console.log(zoom);
+	// 	return {
+	// 		latitude: center.lat(),
+	// 		longitude: center.lng(),
+	// 		latitudeDelta: Math.abs(
+	// 			bounds.getSouthWest().lat() - bounds.getNorthEast().lat()
+	// 		),
+	// 		longitudeDelta: Math.abs(
+	// 			bounds.getSouthWest().lng() - bounds.getNorthEast().lng()
+	// 		),
+	// 		zoom: zoom,
+	// 	};
+	// };
 
-		return {
-			latitude: center.lat(),
-			longitude: center.lng(),
-			// latitudeDelta: Math.abs(
-			// bounds.getSouthWest().lat() - bounds.getNorthEast().lat()
-			// ),
-			// longitudeDelta: Math.abs(
-			// bounds.getSouthWest().lng() - bounds.getNorthEast().lng()
-			// ),
-			zoom: zoom,
-		};
-	};
+	// onDragStart = () => {
+	// 	console.log("i am in onDragStart");
+	// 	const { onRegionChange } = this.props;
 
-	onDragStart = () => {
-		console.log("i am in onDragStart");
-		const { onRegionChange } = this.props;
-
-		if (this.map && onRegionChange) {
-			onRegionChange(this._getCurrentRegion());
-		}
-		console.log(onRegionChange);
-	};
+	// 	if (this.map && onRegionChange) {
+	// 		onRegionChange(this._getCurrentRegion());
+	// 	}
+	// };
 
 	onDragEnd = () => {
-		console.log("i am in onDragEnd");
 		const { onRegionChangeComplete } = this.props;
+		const center = this.map.getCenter();
+
+		const zoom = this.map.getZoom();
 
 		if (this.map && onRegionChangeComplete) {
-			onRegionChangeComplete(this._getCurrentRegion());
+			onRegionChangeComplete({
+				latitude: center.lat(),
+				longitude: center.lng(),
+				zoom: zoom,
+			});
 		}
 	};
 
 	render() {
-		const {
-			region,
-			initialRegion,
-			onRegionChange,
-			onPress,
-			options,
-			defaultZoom,
-		} = this.props;
+		const { region, initialRegion, onPress, options, defaultZoom } = this.props;
 		const { center } = this.state;
 		const style = this.props.style || styles.container;
 
@@ -119,13 +115,7 @@ class MapView extends Component {
 				: initialRegion && initialRegion.latitudeDelta
 				? Math.round(Math.log(360 / initialRegion.latitudeDelta) / Math.LN2)
 				: 15);
-		console.log("Initial region" + JSON.stringify(initialRegion));
-		console.log("region" + JSON.stringify(region));
-		console.log("defaultzoom" + defaultZoom);
-		console.log("state.zoom" + this.state.zoom);
-		console.log("Zoom" + zoom);
 		googleMapProps["zoom"] = zoom;
-		console.log(googleMapProps);
 		return (
 			<View style={style}>
 				<GoogleMapContainer
@@ -137,7 +127,7 @@ class MapView extends Component {
 					// this.setState({ zoom: this.map.getZoom() });
 					// }}
 					{...googleMapProps}
-					onDragStart={this.onDragStart}
+					// onDragStart={this.onDragStart}
 					onIdle={this.onDragEnd}
 					defaultZoom={zoom}
 					onClick={onPress}
